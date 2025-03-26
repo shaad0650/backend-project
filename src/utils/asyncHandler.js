eda5d553d325f87d55a9d6133ccaf35e7a9eee60
shad rasal <shadrasal43@gmail.com>
2025-03-26 12:14:44 +0530

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+// const asyncHandler=()=>{
+
+// }
+// export {asyncHandler}
+// const asyncHandler=(fn)=>async(req,res,next)=>{
+// try{
+
+// }
+// catch(error){
+//     res.status(error.code||500).json({
+//         success:false,
+//         message:error.message
+//     })
+// }
+// }
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+}
